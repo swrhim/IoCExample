6f8b7a934a14fb36e3e69398289a5988db852513
swrhim <swrhim@gmail.com>
2014-03-16 15:03:07 -0400

    Simple Example of IoC using Unity

---
+++ IoCExample/PrintOddNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoCExample
+{
+    class PrintOddNumbers : IPrint
+    {
+        public void PrintNumbers()
+        {
+            Console.WriteLine("1, 3, 5, 7, 9...");
+        }
+    }
+}
---
+++ IoCExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+
+namespace IoCExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer container = new UnityContainer().
+                RegisterType<IPrint, PrintOddNumbers>();
+            var print = container.Resolve<IPrint>();
+            print.PrintNumbers();
+            Console.ReadLine();
+
+        }
+    }
+}
